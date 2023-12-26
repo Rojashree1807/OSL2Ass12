e3d6419432cea8b96c421e56eb597e47d18e0940
Rojashree1807 <rojarojashree1807@gmail.com>
2023-12-26 19:30:44 +0530

    Initial commit

---
+++ WebAppAss12/Controllers/CricketController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAss12.Models;
+
+namespace WebAppAss12.Controllers
+{
+    public class CricketController : Controller
+    {
+        static List<CricketTeam> listCricketTeams = new List<CricketTeam>()
+        {
+           new CricketTeam() { TeamId =1,TeamName="india",NOWC=2},
+           new CricketTeam() { TeamId =2,TeamName="Australia",NOWC=4},
+           new CricketTeam() { TeamId =3,TeamName="Weat Indies",NOWC=2},
+           new CricketTeam() { TeamId =4,TeamName="England",NOWC=1},
+        };
+        // GET: Cricket
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View(new CricketTeam());
+        }
+        [HttpPost]
+        public ActionResult Create(CricketTeam team)
+        {
+            if (ModelState.IsValid)
+            {
+                listCricketTeams.Add(team);
+            }
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult Index()
+        {
+            return View(listCricketTeams);
+        }
+       public ActionResult PAction()
+       {
+            return PartialView("PView");
+       }
+
+    }
+}
---
+++ WebAppAss12/Models/CricketTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAss12.Models
+{
+    public class CricketTeam
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+
+        internal static void Add(CricketTeam team)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
